c379aa006615d610599d481de3b9245c23623b4c
heyou <heyou_0423@163.com>
2020-03-01 19:24:03 +0800

    Initial commit

---
+++ src/main/java/com/heyou/springboot/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,16 @@
+package com.heyou.springboot.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heyou
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchApplication.class, args);
+	}
+
+}
