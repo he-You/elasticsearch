9daf1bff8f9c5f8a2675f1187b4ab41b73b9eebe
heyou-macbook <heyou_0423@163.com>
2020-03-22 21:54:20 +0800

    add:mapper包扫描

--- src/main/java/com/heyou/springboot/elasticsearch/ElasticsearchApplication.java
+++ src/main/java/com/heyou/springboot/elasticsearch/ElasticsearchApplication.java
@@ -1,5 +1,6 @@
 package com.heyou.springboot.elasticsearch;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author heyou
  */
 @SpringBootApplication
+@MapperScan("com.heyou.springboot.elasticsearch.dao")
 public class ElasticsearchApplication {
 
 	public static void main(String[] args) {
