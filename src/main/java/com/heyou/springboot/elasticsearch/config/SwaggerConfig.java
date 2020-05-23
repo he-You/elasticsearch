f0cf7051cf5fe3d6726b93d72ea90998a3c8337e
heyou-macbook <heyou_0423@163.com>
2020-05-23 23:23:08 +0800

    add：swagger配置

---
+++ src/main/java/com/heyou/springboot/elasticsearch/config/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.heyou.springboot.elasticsearch.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/5/23 17:55
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .globalOperationParameters(setHeaderToken())
+                .select() // 选择那些路径和api会生成document
+                .apis(RequestHandlerSelectors.any())// 对所有api进行监控
+                //不显示错误的接口地址
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))//错误路径不监控
+                .paths(PathSelectors.regex("/.*"))// 对根下所有路径进行监控
+                .build();
+    }
+
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ES")
+                .description("es文档")
+                .termsOfServiceUrl("http://www")
+                .version("1.0.0")
+                .build();
+    }
+}
