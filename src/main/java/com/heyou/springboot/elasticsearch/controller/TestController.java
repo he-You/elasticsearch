7ef69b76a42ca9882b3808ca3f45e726912982ee
heyou-macbook <heyou_0423@163.com>
2020-05-23 23:23:24 +0800

    add：swagger-ui

--- src/main/java/com/heyou/springboot/elasticsearch/controller/TestController.java
+++ src/main/java/com/heyou/springboot/elasticsearch/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.heyou.springboot.elasticsearch.controller;
 
 import com.heyou.springboot.elasticsearch.service.TestService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/5/21 23:11
  */
 @RestController(value = "/test")
+@Api(tags = "ES")
 public class TestController {
     @Autowired
     private TestService testService;
--- src/main/java/com/heyou/springboot/elasticsearch/entity/Article.java
+++ src/main/java/com/heyou/springboot/elasticsearch/entity/Article.java
@@ -18,24 +18,24 @@ public class Article {
     /**
      * 主键ID
      */
-    @Field(type = FieldType.Keyword)
+    //@Field(type = FieldType.Keyword)
     private String id;
 
     /**
      * 文章标题
      */
-    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
+    //@Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
     private String title;
 
     /**
      * 文章内容
      */
-    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
+    //@Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
     private String content;
 
     /**
      * 创建时间
      */
-    @Field(type = FieldType.Date, pattern = "yyyy-MM-dd HH:mm:ss", format = DateFormat.custom)
+    //@Field(type = FieldType.Date, pattern = "yyyy-MM-dd HH:mm:ss", format = DateFormat.custom)
     private String createTime;
 }
