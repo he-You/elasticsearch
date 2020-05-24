2c170d65b50ce43d6797423f465236f747ca873a
heyou-macbook <heyou_0423@163.com>
2020-05-24 18:25:22 +0800

    add:新增 ES 通过关键字查询数据集合

--- src/main/java/com/heyou/springboot/elasticsearch/dao/IBlogRepository.java
+++ src/main/java/com/heyou/springboot/elasticsearch/dao/IBlogRepository.java
@@ -3,9 +3,12 @@ package com.heyou.springboot.elasticsearch.dao;
 import com.heyou.springboot.elasticsearch.entity.Blog;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 /**
  * @author heyou(heyou_0423 @ 163.com)
  * @date 2020/5/24 10:37
  */
 public interface IBlogRepository extends ElasticsearchRepository<Blog,String> {
+    List<Blog> findByTitleLike(String keyword);
 }
